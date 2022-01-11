e82813a1bf3bf928f7bfbe84eba477641e754a45
Marek Brandt <marek7119@gmail.com>
2022-01-11 08:54:00 +0100

    menu is nearly completed

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -37,11 +37,49 @@ def change_board_settings(size, in_line):
             option = int(option)
             if 1 <= option <= 3:
                 if option == 1:
-                    input("Type size of the board")
+                    size = change_board_size()
+                elif option == 2:
+                    in_line = change_win_condition(size)
+                else:
+                    return size, in_line
             else:
                 print("Type correct number")
         except ValueError:
             print("Type number")
+
+
+def change_board_size():
+    run = True
+    while run:
+        new_size = input("Type size of the board: ")
+        try:
+            new_size = int(new_size)
+            if 3 <= new_size < 11:
+                print("Actual board size is " + str(new_size) + "x" + str(new_size))
+                return new_size
+            else:
+                print("Try a number in range 3 to 10")
+        except ValueError:
+            print("Type correct number")
+
+
+def change_win_condition(size):
+    run = True
+    while run:
+        in_line = input("Type the length of winning line: ")
+        try:
+            in_line = int(in_line)
+            if 2 <= in_line <= size:
+                print("Changes accepted")
+                return in_line
+            else:
+                print("Try a number in range 3 to size of the board, which is: " + str(size))
+        except ValueError:
+            print("Type correct number")
+
+
+def change_nicknames(nickname1, nickname2):
+    pass  # todo
 
 
 def main():
@@ -50,26 +88,18 @@ def main():
     nickname1 = "Player1"
     nickname2 = "Player2"
 
-    option = menu_options()
-
-    if option == 1:
-        Game(size, in_line, nickname1, nickname2)
-    elif option == 2:
-        change_board_settings(size, in_line)
-
     run = True
     while run:
-        size = input("Type the width of the board: ")
-        try:
-            size = int(size)
-            if 3 <= size < 11:
-                print("Creating a board " + str(size) + "x" + str(size))
-                run = False
-            else:
-                print("Try a number in range 3 to 10")
-        except ValueError:
-            print("You have to type a number")
-    game = Game(size)
+        option = menu_options()
+        if option == 1:
+            Game(size, in_line, nickname1, nickname2)
+        elif option == 2:
+            size, in_line = change_board_settings(size, in_line)
+        elif option == 3:
+            change_nicknames(nickname1, nickname2)
+        else:
+            print("Thank you for playing!")
+            run = False
 
 
 if __name__ == '__main__':
